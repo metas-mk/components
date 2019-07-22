8b2147b74a4bf4cfc0ad6309427ec16afe7d4be1
metas-mk <mark.krake@metasfresh.com>
2019-07-22 19:23:49 +0200

    Moved faker image to props

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" date="Today at 6:00PM" text="A nice blog!" />
-            <CommentDetail author="John" date="Monday at 5:22PM" text="Can be more precise." />
-            <CommentDetail author="Peter" date="Friday at 11:55PM" text="Some people like it." />
-            <CommentDetail author="Mary" date="Sunday at 3:19PM" text="Dunno" />
+            <CommentDetail author="Sam" date="Today at 6:00PM" text="A nice blog!" avatar={faker.image.avatar()} />
+            <CommentDetail author="John" date="Monday at 5:22PM" text="Can be more precise."  avatar={faker.image.avatar()} />
+            <CommentDetail author="Peter" date="Friday at 11:55PM" text="Some people like it."  avatar={faker.image.avatar()} />
+            <CommentDetail author="Mary" date="Sunday at 3:19PM" text="Dunno"  avatar={faker.image.avatar()} />
         </div>
     ); 
 };

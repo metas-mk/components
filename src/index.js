8312fdf99263950ed2f91a39471f0f04f7e6da5a
metas-mk <mark.krake@metasfresh.com>
2019-07-22 19:09:04 +0200

    R2D3 Using react props system

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" date="Today at 6:00PM" text="A nice blog!" />
+            <CommentDetail author="John" date="Monday at 5:22PM" text="Can be more precise." />
+            <CommentDetail author="Peter" date="Friday at 11:55PM" text="Some people like it." />
+            <CommentDetail author="Mary" date="Sunday at 3:19PM" text="Dunno" />
         </div>
     ); 
 };
